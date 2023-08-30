b4c18fa93c6e5713a2846c7e8ce5e2bc69c69220
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-08-30 14:49:09 -0400

    Complete

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -5,9 +5,16 @@ class GetRequester:
 
     def __init__(self, url):
         self.url = url
-
+        
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        program_list = []
+        response_content = self.get_response_body()
+        response = json.loads(response_content)
+        for program in response:
+            program_list.append(program)
+
+        return program_list
